7c08bba6139c7a04c83e72883b9d30db534ae6aa
ninad-debug <125790058+ninad-debug@users.noreply.github.com>
2023-05-13 12:53:00 +0530

    Integrating Context and Reducer

---
+++ src/pages/Cart.js
@@ -0,0 +1,20 @@
+import { useCart } from "../context/CartContext";
+import { useTitle } from "../hooks/useTitle";
+import { CartCard } from "../components";
+
+export const Cart = () => {
+  const {total, cartList} = useCart();
+  console.log(cartList);
+  useTitle("Cart");
+
+  return (
+    <main>
+      <section className="cart">
+        <h1>Cart Items: {cartList.length} / ${total}</h1>
+        { cartList.map((product) => (
+          <CartCard key={product.id} product={product} />
+        )) }        
+      </section>
+    </main>
+  )
+}
